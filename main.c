22788f9764478920963581c2688205651789e3bb
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-05-20 10:04:45 +0100

    setup simple shell exit code test with valgrind 🔥

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Building simple shell hsh program ...\n");
+	return (0);
+}
